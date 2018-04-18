f4551591e6bffe5c24787ef36831251c3109f5e8
Jacob Castello <jacob.castello@cfpb.gov>
2018-04-17 21:56:57 -0400

    Display requirments file

--- this.sh
+++ this.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-python3 -m virtualenv .venv#!/usr/bin/env bash
+python3 -m virtualenv .venv
 virtualenv .venv && source .venv/bin/activate
 pip install -r requirements_to_lock.txt
 pip freeze > requirements.txt
-
+cat requirements.txt
 # Now with pipenv
 git checkout pipenv
 pipenv install --three
